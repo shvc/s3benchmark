7efbce807e9fb5c17396ddf6157e0aae72f866db
shvc <chencheng@myshare.io>
2020-08-06 14:57:17 +0800

    refine test code

--- s3benchmark_test.go
+++ s3benchmark_test.go
@@ -8,8 +8,8 @@ import (
 
 func Test_hmacSHA1(t *testing.T) {
 	cases := map[string][2]string{
-		"46b4ec586117154dacd49d664e5d63fdc88efb51": [2]string{"foo", "bar"},
-		"68d880c3499b7e53e6ef5b0bd92ad723244fc591": [2]string{"s3", "bucket"},
+		"46b4ec586117154dacd49d664e5d63fdc88efb51": {"foo", "bar"},
+		"68d880c3499b7e53e6ef5b0bd92ad723244fc591": {"s3", "bucket"},
 	}
 	for result, ti := range cases {
 		to := fmt.Sprintf("%x", hmacSHA1([]byte(ti[0]), ti[1]))
